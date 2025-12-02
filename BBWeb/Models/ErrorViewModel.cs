a3098405805275c3aed7d8ff877ccb9a93491c9b
Your Name <your.email@example.com>
2025-12-02 23:07:29 +0400

    Add project files.

---
+++ BBWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BBWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
