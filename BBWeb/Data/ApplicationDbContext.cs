93818a11d6b54e1b2f97886f8272a7e0fc9a8724
Your Name <your.email@example.com>
2025-12-03 21:21:18 +0400

    Database Create Table

--- BBWeb/Data/ApplicationDbContext.cs
+++ BBWeb/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BBWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BBWeb.Data
 {
@@ -10,6 +11,6 @@ namespace BBWeb.Data
 
         }
 
-
+        public DbSet<Category> Categories { get; set; }
     }
 }
