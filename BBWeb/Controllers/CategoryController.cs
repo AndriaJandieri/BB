f514ceb9540d19a78ed9bfc748f7e1bca94eb05c
Your Name <your.email@example.com>
2025-12-05 13:52:58 +0400

    db is connected, working on Create

---
+++ BBWeb/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using BBWeb.Data;
+using BBWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BBWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
