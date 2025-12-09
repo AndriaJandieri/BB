94253334d812230ff07b200eaf22831082c76cdf
Andria Jandieri <jandieri.andria@gmail.com>
2025-12-09 22:09:10 +0400

    Razor Pages Project added in Solution

---
+++ BB_Razor/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using BB_Razor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BB_Razor.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+
+        }
+
+        public DbSet<Category> CategoriesRazor { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "ActionR", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFiR", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "HistoryR", DisplayOrder = 3 }
+            );
+        }
+    }
+}
